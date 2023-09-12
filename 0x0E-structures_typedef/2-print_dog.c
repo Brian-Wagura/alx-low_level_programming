ec31c7bcd068f306b08b5177e45434e55de63265
Brian-Wagura <briankiraguri@gmail.com>
2023-09-12 13:42:44 -0700

    Modified dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * print_dog - Prints elements of d
+ * @d: argument character
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %f\n", d->age);
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
