7699d1f8a0d2e0ffb19f502cf935621b65d3a1af
Brian-Wagura <briankiraguri@gmail.com>
2023-11-24 00:42:42 -0800

    betty comments putchar

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,5 +1,13 @@
 #include <unistd.h>
 
+/**
+ * _putchar - writes the char c to stdout
+ * @c: The char to print
+ *
+ * Return: On Success - 1
+ * OnError - -1 is returned
+ */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
