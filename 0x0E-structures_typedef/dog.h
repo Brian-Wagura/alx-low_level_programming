86375842d09d1dfe785e41880cb1acf8e47f2b2a
Brian-Wagura <briankiraguri@gmail.com>
2023-11-27 06:50:24 -0800

    Modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+/**
+ * struct dog - a dog struct with name, age, owner
+ *
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
