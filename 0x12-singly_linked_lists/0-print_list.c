02c59f95ddff08c5df242a2477a25e4875158281
Brian-Wagura <briankiraguri@gmail.com>
2023-12-08 14:20:41 -0800

    Print linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,23 +12,14 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-		return (count);
-	}
-
-	printf("[");
 	while (h != NULL)
 	{
-		printf("%s", h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
-		if (h != NULL)
-		{
-			printf(", ");
-		}
 	}
-	printf("]\n");
 	return (count);
 }
